078791a176e4ab4b51a1f55ee7cfce165a0eaae7
Marcelo Diniz <marcelo.diniz.ti@gmail.com>
2023-07-26 15:51:52 -0300

    ACloudGuru

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
-output "storage_account_id" {
-  value       = azurerm_storage_account.storage.id
-  description = "The storage account ID"
-}
+# output "storage_account_id" {
+#   value       = azurerm_storage_account.storage.id
+#   description = "The storage account ID"
+# }
 
 
